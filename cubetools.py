8891a594afbd73211ab30d7d08af173102edcb85
Niccolo Ricardi <niccolo.ricardi@unige.ch>
2020-03-16 15:56:47 +0100

    cube from dm, to test

--- cubetools.py
+++ cubetools.py
@@ -75,7 +75,7 @@ class cubefile:
         import copy as c
         return c.deepcopy(self)
     
-    def from_file(fname):
+    def from_file(fname, emptyval = False):
         """
         Note
         ----
@@ -88,6 +88,84 @@ class cubefile:
         """
         return read_cubefile(fname)
     
+    def from_grid_specs(g = None, Np_vect = None, Vect_M = None,  o = None, comment="Empty function values\nOnly grid\n"):
+        """
+        Note
+        ----
+        Returns an empty-values cubefile object, useful to then fill this with values from a density matrix, or to store grid specs
+        
+        Parameters
+        ----------
+        g: geom or str
+            either geom object or string of the xyz filename
+        Np_vect: array(3,)
+            number of points along x,y,z
+        Vect_M: array(3,3)
+            grid vectors. Generally diagonal
+        o: array(3,)
+            origin of the grid. default is 0,0,0
+        comment: str
+            optional comment.
+        
+        Returns
+        -------
+        cubefile obj
+            Cubefile with desired grid but empty value array
+        """
+        return get_emptyval_cube(g = g, Np_vect = Np_vect, Vect_M = Vect_M,  o = o, comment = comment)
+    
+    def get_cubevals_from_dm(self, dmfile = "", basfile = "", comment = "Obtained from density matrix in file: \n {dmfile} \n"):
+        """
+        Note
+        ----
+        Allows to obtain values from a density matrix.
+        
+        Parameters
+        ----------
+        dmfile: str
+            density matrix file
+        basfile: str
+            basis .nwchem file
+        comment: str
+            default writes dmfile, use empty string to leave self.cube
+        Sets
+        ----
+        self.values
+            obtained from density matrix
+        self.comment
+            user-chosen, default is corresponding density matrix
+        """
+        from pyscf import gto
+        from pyscf.dft.numint import eval_ao, eval_rho
+        import dmtools.dmtools as dmt
+        if dmfile == "":
+            raise FileNotFoundError("You must specify the density matrix file. Can have header or not, can be alpha and beta or not") #TODO check if it reads a single DM as 2*a or a.
+        if basfile == "":
+            import glob as gl
+            files = gl.glob("*.nwchem")
+            if len(files) == 0:
+                raise FileNotFoundError("no *.nwchem file! basis must be in .nwchem!")
+            elif len(files) == 1:
+                basfile = files[0]
+                print("basfile not specified, using {}".format(basfile))
+            else: 
+                basfile = input("Too many *.nwchem. Type the one to use")
+        self.geom.change_coord_unit("Angstrom")
+        geomstring = self.geom.__str__()
+        with open(basfile, "r") as f:
+            ibasis = f.read()
+        mol = gto.M(atom = geomstring, basis = ibasis)
+        dm_obj = dmt.DM.from_dmfile(dmfile)
+        dm = dm_obj.get_dm_full()
+        self.g.change_coord_unit("au")
+        points = self.get_coordlist()
+        ao_mol = eval_ao(mol, points, deriv=0)
+        cubevals= eval_rho(mol, ao_mol, dm, xctype='LDA')
+        self.values = cubevals.reshape(*self.Np_vect)
+        if comment != "":
+            self.comment = comment.format(dmfile=dmfile)
+        
+        
     def write(self, fname, digits=5):
         """
         Note
@@ -102,6 +180,12 @@ class cubefile:
             digits desired. default is 5
         """
         import mendeleev as md
+        if len(self.comment.split("\n"))==3:
+            pass
+        elif len(self.comment.split("\n")) < 3:
+            self.comment+="\n"*(3-len(self.comment.split("\n")))
+        elif len(self.comment.split("\n")) > 3:
+            raise otherError("Please retry with a two- or fewer-lines comment")
         with open(fname, "w") as out:
             out.write(self.comment)
             out.write("{:5}{:12.6f}{:12.6f}{:12.6f}\n".format(len(self.geom.atoms),self.origin[0],self.origin[1],self.origin[2]))
@@ -109,7 +193,8 @@ class cubefile:
                 out.write("{:5}{:12.6f}{:12.6f}{:12.6f}\n".format(self.Np_vect[i],self.Vect_M[i][0],self.Vect_M[i][1],self.Vect_M[i][2]))
             for i in range(len(self.geom.atoms)):
 #                out.write(self.geom.atoms[i]+' {:{w}.{p}f} {:{w}.{p}f} {:{w}.{p}f}'.format(g.coords(which)[i][0],g.coords(which)[i][1],g.coords(which)[i][2],w=spacing+decs+1, p=decs)+"\n")
-                out.write('{:5}{:12.6f}{:12.6f}{:12.6f}{:12.6f}\n'.format(md.element(self.geom.atoms[i]).atomic_number,md.element(self.geom.atoms[i]).atomic_number,self.geom.inp_coords[i][0],self.geom.inp_coords[i][1],self.geom.inp_coords[i][2]))
+                out.write('{:5}{:12.6f}{:12.6f}{:12.6f}{:12.6f}\n'.format(md.element(self.geom.atoms[i]).atomic_number,md.element(self.geom.atoms[i]).atomic_number,self.geom.coords[i][0],
+                          self.geom.coords[i][1],self.geom.coords[i][2]))
             for x in range(self.Np_vect[0]):
                 for y in range(self.Np_vect[1]):
                     for z in range(self.Np_vect[2]):
@@ -124,11 +209,11 @@ class cubefile:
         ----
         Allows easy printing
         """
-        geomstring = self.geom.__str__
-        Npoints = "Npoints: x={},y={},z={}".format([self.Np_vect[i] for i in range(3)])
-        isdiag = (np.diag(np.diag(Vect_M))== Vect_M).all()
-        vect = "v_x={}, vy={}, vz={}".format([np.diag(Vect_M)[i] for i in range(3)]) if isdiag else Vect_M
-        orig = "origin: {}, {}, {}".format([self.origin[i] for i in range(3)])
+        geomstring = self.geom.__str__()
+        Npoints = "Npoints: x = {}, y = {}, z = {}".format(*tuple([self.Np_vect[i] for i in range(3)]))
+        isdiag = (np.diag(np.diag(self.Vect_M)) == self.Vect_M).all()
+        vect = "v_x = {}, vy = {}, vz = {}".format(*tuple([np.diag(self.Vect_M)[i] for i in range(3)])) if isdiag else self.Vect_M
+        orig = "origin: {}, {}, {}\n".format(*tuple([self.origin[i] for i in range(3)]))
         return "\n".join([geomstring,Npoints,vect,orig,self.comment])
     
     def __repr__(self, spacing=4, decs=6):
@@ -137,11 +222,11 @@ class cubefile:
         ----
         Allows easy calling
         """
-        geomstring = self.geom.__str__
-        Npoints = "Npoints: x={},y={},z={}".format([self.Np_vect[i] for i in range(3)])
-        isdiag = (np.diag(np.diag(Vect_M))== Vect_M).all()
-        vect = "v_x={}, vy={}, vz={}".format([np.diag(Vect_M)[i] for i in range(3)]) if isdiag else Vect_M
-        orig = "origin: {}, {}, {}".format([self.origin[i] for i in range(3)])
+        geomstring = self.geom.__str__()
+        Npoints = "Npoints: x = {}, y = {}, z = {}".format(*tuple([self.Np_vect[i] for i in range(3)]))
+        isdiag = (np.diag(np.diag(self.Vect_M)) == self.Vect_M).all()
+        vect = "v_x = {}, vy = {}, vz = {}".format(*tuple([np.diag(self.Vect_M)[i] for i in range(3)])) if isdiag else self.Vect_M
+        orig = "origin: {}, {}, {}\n".format(*tuple([self.origin[i] for i in range(3)]))
         return "\n".join([geomstring,Npoints,vect,orig,self.comment])
     
     def __add__(self, other):
@@ -196,7 +281,7 @@ class cubefile:
         cubefile
             the product
         """
-        return times(self,other)    
+        return times(self,factor)    
      
     def __iadd__(self,other):
         """
@@ -237,7 +322,7 @@ class cubefile:
         factor: float
             multiplying factor
         """
-        self.times(other)
+        self.times(factor)
         return self  
         
     def check_same_grid(self,cf):
@@ -245,7 +330,7 @@ class cubefile:
         Parameters
         ----------
         cf: cubefile
-            cf object to compare grid of
+            cubefile object to compare grid of
         """
         check_same_grid(self,cf)            
             
@@ -366,7 +451,7 @@ class cubefile:
     def get_coordlist(self):
         """
         Returns
-        -------self.check_same_grid(cf)
+        -------
         the coordinates of the whole grid
         """
         return self.get_coords(self.get_pointlist())
@@ -388,29 +473,30 @@ class cubefile:
             d[n]=np.linalg.norm(i-point)
         return d.min()
         
-def read_cubefile(fname):
+def read_cubefile(fname, empty_values = False):
     """
     Parameters
     ----------
     fname: str
         filename or path 
-    
+    empty_values: bool
+        False => read all file, True => read only grid, zeros for values
     Returns
-    -------self.check_same_grid(cf)
+    -------
     cubefile 
-        cf object
+        cubefile object
     """
     import mendeleev as md
     pnums = []
-    V_m=[]
-    cmmnt=""
+    V_m = []
+    cmmnt = ""
     natm = 1        
     positions = []
-    atms=[]
+    atms = []
     with open(fname) as f:
         for j,line in enumerate(f):
             if j < 2:
-                cmmnt+=line
+                cmmnt += line
             if j == 2:
                 splt =line.split()
                 natm = int(splt[0])
@@ -426,30 +512,88 @@ def read_cubefile(fname):
             if j == natm+6:
                 break
     Np_vect = np.asarray(list(map(int,pnums)))
-    Vect_M=np.asarray(V_m)
-    coords=np.asarray(positions)
-    atoms=np.asarray(atms)
-    g=geom.geom(atoms,coords, coord_unit="au")
-    with open(fname) as f:
-        cube = f.read()
-    cubelist=[]
-    pattern2 = '(-?\d+\.?\d+[Ee]\ *[-+]?\ *\d+\s*){' + re.escape(str(pnums[2])) + '}'
-    cubelist.append([])
-    xcnt = 0
-    ycnt = 0
-    for xy,m in enumerate(re.finditer(pattern2,cube)):
-        if xy % Np_vect[1] == 0 and xy > 0:
-            cubelist[xcnt]=np.asarray(cubelist[xcnt])
-            xcnt += 1 if xy>0 else 0
-            ycnt = 0
-            cubelist.append([]) # append empty list for x
-        s = m.group().split()
-        s = np.asarray(list(map(float,s)))
-        cubelist[xcnt].append(s)
-        ycnt += 1
-    cubelist[-1]=np.asarray(cubelist[-1])
-    cubearray=np.asarray(cubelist)
+    Vect_M = np.asarray(V_m)
+    coords = np.asarray(positions)
+    atoms = np.asarray(atms)
+    g = geom.geom(atoms,coords, coord_unit="au")
+    if not empty_values:
+        with open(fname) as f:
+            cube = f.read()
+        cubelist = []
+        pattern2 = '(-?\d+\.?\d+[Ee]\ *[-+]?\ *\d+\s*){' + re.escape(str(pnums[2])) + '}'
+        cubelist.append([])
+        xcnt = 0
+        ycnt = 0
+        for xy,m in enumerate(re.finditer(pattern2,cube)):
+            if xy % Np_vect[1] == 0 and xy > 0:
+                cubelist[xcnt]=np.asarray(cubelist[xcnt])
+                xcnt += 1 if xy>0 else 0
+                ycnt = 0
+                cubelist.append([]) # append empty list for x
+            s = m.group().split()
+            s = np.asarray(list(map(float,s)))
+            cubelist[xcnt].append(s)
+            ycnt += 1
+        cubelist[-1] = np.asarray(cubelist[-1])
+        cubearray = np.asarray(cubelist)
+    else:
+        cubearray = np.zeros(list(Np_vect))
     return cubefile(g,Np_vect,Vect_M,cubearray,origin=o,comment=cmmnt)
+
+def get_emptyval_cube(g = None, Np_vect = None, Vect_M = None,  o = None, comment = "Empty function values\nOnly grid\n"):
+    """
+    Note
+    ----
+    Allows to get a cubefile object with grid specs but empty values
+    
+    Parameters
+    ----------
+    g: geom obj or str
+        geometry for the cubefile object or .xyz file to obtain it
+    Np_vect: array(3,)
+        number of points along x,y,z
+    Vect_M: array(3,3)
+        grid vectors. Generally diagonal
+    o: array(3,)
+        origin of the grid. default is 0,0,0
+    comment: str
+        comment, default specifies it is empty
+    """
+    if Np_vect == None:
+        raise gridError("You must specify Np_vect")
+    if Vect_M == None:
+        raise gridError("You must specify Vect_M")
+    if o == None:
+        raise gridError("You must specify the origin")
+    if g == None:
+        raise Error("You must specify the geometry. Either geom object of .xyz filename")
+    elif type(g) == str:
+        g = geom.geom.from_xzy(g)
+    elif type(g) != geom.geom: #TODO check type for geom obj
+        raise TypeError("the geometry is neither a geometry object nor a file!")
+    values = np.zeros(list(Np_vect))
+    g.change_coord_unit("au")
+    return cubefile(g, Np_vect, Vect_M, values,o, comment = comment)
+    
+def get_cube_from_dm(dmfile = "", basfile = "", g = None, Np_vect = None, Vect_M = None,  o = None):
+    """
+    Parameters
+    ----------
+    dmfile: str
+        density matrix file
+    basfile: str
+        basis .nwchem file
+    comment: str
+        default writes dmfile, use empty string to leave self.cube
+        
+    Returns
+    -------
+    cubefile object
+        cube from the density matrix
+    """
+    cube = get_emptyval_cube(g = g, Np_vect = Np_vect, Vect_M = Vect_M, o = o)
+    cube.get_cubevals_from_dm(dmfile = dmfile, basfile = basfile)
+    return cube
 
 def have_same_grid(cf1,cf2,orig_thresh=1e-5, printout=False):
     """
@@ -540,7 +684,7 @@ def subtract(cf1,cf2,comment="subtracted with cubetools\n here it goes\n",geom="
     Returns
     -------
     cubefile
-        cf object with difference
+        cubefile object with difference
     """
     if comment !="subtracted with cubetools\n here it goes\n":
         if len(comment.split("\n"))==3:
@@ -585,7 +729,7 @@ def add(cf1,cf2,comment="added with cubetools\n here it goes\n",geom="",Continue
     Returns
     -------
     cubefile
-        cf object with addition
+        cubefile object with addition
     """
     if comment !="added with cubetools\n here it goes\n":
         if len(comment.split("\n"))==3:
@@ -600,7 +744,7 @@ def add(cf1,cf2,comment="added with cubetools\n here it goes\n",geom="",Continue
         Np_vect=cf1.Np_vect
         Vect_M=cf1.Vect_M
         origin=cf1.origin
-
+    else:
         if Continue:
             print("Not the same grid! creating mock-object and continuing")
             Np_vect=cf1.Np_vect
@@ -623,9 +767,9 @@ def times(cf, factor):
     Returns
     -------
     cubefile
-        cf object where values have been multiplied
+        cubefile object where values have been multiplied
     """
-    copy=cf.copy()
+    copy = cf.copy()
     copy.times(factor)
     return copy
         
